601b3a05d942c56dc1b6ba3b9b7d8eabeb045bbb
Uğur Aydoğan <ugur.aydogan@sirenbilisim.com.tr>
2023-02-22 09:24:54 +0300

    Add WebSocket

---
+++ src/main/java/com/example/demo/model/Message.java
@@ -0,0 +1,20 @@
+package com.example.demo.model;
+
+public class Message {
+
+    private String content;
+    public String getMessage() {
+        return content;
+    }
+
+    public void setMessage(String content) {
+        this.content = content;
+    }
+
+    public Message(String content) {
+        this.content = content;
+    }
+
+
+
+}
